3bc255238f95bfaaf77ee3b922944bf91a1c336f
Blake La Pierre <blakelapierre@gmail.com>
2014-05-16 17:21:57 -0700

    The Drop Target

--- site/src/frontend/app.js
+++ site/src/frontend/app.js
@@ -3,3 +3,5 @@ var angular = require('angular');
 module.exports = angular.module('instantFile', [])
 
   .directive('instantFile',   require('./instantFile/directive'))
+
+  .directive('fileDropArea',  require('./fileDropArea/directive'))
---
+++ site/src/frontend/fileDropArea/directive.js
@@ -0,0 +1,29 @@
+module.exports = function fileDropArea() {
+  
+  function processDragOverEnter(e) {
+    e.preventDefault();
+    e.dataTransfer.effectAllowed = 'copy';
+    return false;
+  };
+
+  return {
+    restrict: 'A',
+    scope: {
+      'file': '=fileDropArea'
+    },
+    link: function($scope, element, attributes) {
+      element.bind('dragover', processDragOverEnter);
+      element.bind('dragenter', processDragOverEnter);
+
+      element.bind('drop', function(e) {
+        e.preventDefault();
+
+        $scope.file = e.dataTransfer.files[0];
+
+        console.log($scope.file);
+
+        return false;
+      });
+    }
+  }
+};
