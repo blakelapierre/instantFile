efd01b50179c69335e3e9e8e973b101d8194cc22
Blake La Pierre <blakelapierre@gmail.com>
2014-05-18 13:08:30 -0700

    Switching to :80 for instafile.io launch.
    Might want to enable TLS as well? Will have to figure out how to. We
    got a free SSL certificate from gandi.net.

--- site/src/app.js
+++ site/src/app.js
@@ -39,7 +39,7 @@ function getPublicAddress (deliver) {
 module.exports = startServer;
 
 startServer({
-  port: 2777,
+  port: 80,
   rtcport: 2776,
   serverRoot: __dirname,
   repoLocation: path.join(__dirname, './../../'),
