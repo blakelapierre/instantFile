ee27b16d7c84fc9d3cfe16818c67394cf996a8ef
Blake La Pierre <blakelapierre@gmail.com>
2014-05-18 12:34:53 -0700

    Not really sure what this is, but it tests out fine for now.

--- site/src/frontend/directives/teaser/directive.js
+++ site/src/frontend/directives/teaser/directive.js
@@ -1,6 +1,18 @@
 module.exports = function() {
   return {
     restrict: 'E',
-    template: require('./template.html')
+    template: require('./template.html'),
+    link: function($scope, element, attributes) {
+      window.fitText(element.children()[0]);
+      // function resize() {
+      //   console.log(element);
+      //   element.css('font-size', '200px');
+      // }
+
+      // resize();
+
+      // window.addEventListener('resize', resize);
+      // window.addEventListener('orientationchange', resize);
+    }
   }
 };
