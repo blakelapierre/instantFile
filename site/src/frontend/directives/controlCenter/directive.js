edf314ff94a03f840e25c458b7cd3b4347f12520
Blake La Pierre <blakelapierre@gmail.com>
2014-05-19 16:27:11 -0700

    Why not let the browser handle the email?
    mailto:

--- site/src/frontend/directives/controlCenter/directive.js
+++ site/src/frontend/directives/controlCenter/directive.js
@@ -2,11 +2,22 @@ module.exports = function controlCenterDirective() {
   return {
     restrict: 'E',
     template: require('./template.html'),
+    scope: {},
+    link: function($scope, element, attributes) {
+      var shareButton = element.find('a');
+      window.fitText(shareButton);
+
+    },
     controller: ['$scope', 'host', function($scope, host) {
       if (host.file) {
         $scope.showInfo = true;
       }
       $scope.currentUrl = window.location.toString();
+
+      var fileName = host.file ? host.file.name : '';  // We probably shouldn't even do this
+
+      $scope.emailSubject = encodeURI('I want to send you ' + fileName);
+      $scope.emailBody = encodeURI('Get it from me at ' + $scope.currentUrl);
     }]
   };
 };
