127a4c9514d0bad4ba8562027d6ba5e48688310b
Blake La Pierre <blakelapierre@gmail.com>
2014-05-19 15:26:39 -0700

    Full-screen button has shipped.

--- site/src/frontend/directives/instantFile/directive.js
+++ site/src/frontend/directives/instantFile/directive.js
@@ -6,11 +6,25 @@ module.exports = function instantFileDirective() {
 
     },
     controller: ['$scope', '$location', 'host', function($scope, $location, host) {
+      $scope.selectFile = function() {
+        var input = document.createElement('input');
+        input.type = 'file';
+
+        angular.element(input).bind('change', function(e) {
+          host.file = e.target.files[0];
+          $location.path(host.file.name);
+
+          $scope.$digest(); // Necessary for $location.path to actually do something
+        });
+
+        input.click();
+      };
+
       $scope.$watch('droppedFile', function(droppedFile) {
         host.file = droppedFile;
         
         if (droppedFile) {
-          $location.path(droppedFile.name);
+          $location.path(host.file.name);
         }
       });
     }]
