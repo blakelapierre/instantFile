0c6691b796108b806a90e52b3a4c008f11da103c
Blake La Pierre <blakelapierre@gmail.com>
2014-05-19 17:54:52 -0700

    Basic, anonymous, stats at http://instafile.io/stats

--- site/src/server/server.js
+++ site/src/server/server.js
@@ -12,5 +12,22 @@ module.exports = function(config, callback) {
   var webserver = app.listen(config.port),
       rtcManager = webRTC.listen(config.rtcport);
 
+  var router = express.Router();
+
+  router.get('/stats', function(req, res) {
+    var socketCount = 0,
+        roomCount = 0;
+
+    for (var socket in rtcManager.rtc.sockets) socketCount++;
+    for (var room in rtcManager.rtc.rooms) roomCount++;
+
+    res.json({
+      sockets: socketCount,
+      rooms: roomCount
+    });
+  });
+
+  app.use('/', router);
+
   callback(webserver, rtcManager);
 };
