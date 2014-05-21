d331777f6c53669ff99140cb6afaf790611be5a9
Blake La Pierre <blakelapierre@gmail.com>
2014-05-20 21:31:08 -0700

    Moderate speed boost Moderate speed boost.

--- site/src/frontend/factories/rtc/factory.js
+++ site/src/frontend/factories/rtc/factory.js
@@ -23,7 +23,7 @@ module.exports = ['$location', function($location) {
   };
 
   function sendFile(channel, file, progress) {
-    var chunkSize = 16 * 1024,
+    var chunkSize = 64 * 1024,
         reader = new FileReader(),
         stats = {};
 
@@ -34,6 +34,7 @@ module.exports = ['$location', function($location) {
 
       var offset = 0,
           backoff = 0,
+          iterations = 1,
           startTime = new Date().getTime();
 
       stats.startTime = startTime;
@@ -42,29 +43,32 @@ module.exports = ['$location', function($location) {
       stats.speed = 0;
 
       function sendChunk() {
-        var now = new Date().getTime(),
-            size = Math.min(chunkSize, buffer.byteLength - offset),
-            chunk = buffer.slice(offset, offset + size);
+        for (var i = 0; i < iterations; i++) {
+          var now = new Date().getTime(),
+              size = Math.min(chunkSize, buffer.byteLength - offset),
+              chunk = buffer.slice(offset, offset + size);
 
-        try {
-          channel.send(chunk);
-          offset += size;
-          stats.sent = offset;
-          stats.speed = offset / (now - startTime) * 1000;  
-          stats.progress = stats.sent / stats.total;
+          try {
+            channel.send(chunk);
+            offset += size;
+            stats.sent = offset;
+            stats.speed = offset / (now - startTime) * 1000;  
+            stats.progress = stats.sent / stats.total;
 
-          backoff = 0;
-          stats.backoff = backoff;
+            backoff = 0;
+            stats.backoff = backoff;
+            iterations++;
+          } catch(e) {
+            backoff += 100;
+            stats.backoff = backoff;
+            iterations = 1;
+            break; // get me out of this for loop!
+          }
 
-          if (offset < buffer.byteLength) setTimeout(sendChunk, 0);
-        } catch(e) {
-          console.log(e);
-          if (offset < buffer.byteLength) setTimeout(sendChunk, backoff);
-          backoff += 100;
-          stats.backoff = backoff;
+          if (progress) progress(stats);
         }
 
-        if (progress) progress(stats);
+        if (offset < buffer.byteLength) setTimeout(sendChunk, backoff);
       };
 
       sendChunk();
