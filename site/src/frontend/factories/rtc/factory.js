89e12daa27953d35d8413bb92331997417e7ebd8
Blake La Pierre <blakelapierre@gmail.com>
2014-05-21 02:11:48 -0700

    Second try.

--- site/src/frontend/factories/rtc/factory.js
+++ site/src/frontend/factories/rtc/factory.js
@@ -4,7 +4,7 @@ module.exports = ['$location', function($location) {
   function launchCommandCenter(room, callback) {
     console.log(room);
 
-    var url = 'https://' + $location.host();
+    var url = 'wss://' + $location.host();
 
     // need to handle subsequent calls of this function correctly
     webRTC.connect(url, room);
